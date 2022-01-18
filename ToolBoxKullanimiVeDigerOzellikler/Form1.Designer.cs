f0908f59cea928032e3dfb5c9c731bbfb38c08fb
husey <husey@MYC>
2022-01-18 22:33:18 +0300

    Add project files.

---
+++ ToolBoxKullanimiVeDigerOzellikler/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ToolBoxKullanimiVeDigerOzellikler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.DateTimeDogum = new System.Windows.Forms.DateTimePicker();
+            this.ComboCins = new System.Windows.Forms.ComboBox();
+            this.ListPersonel = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(23, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad";
+            // 
+            // txtAd
+            // 
+            this.txtAd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(23, 92);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(162, 34);
+            this.txtAd.TabIndex = 1;
+            // 
+            // DateTimeDogum
+            // 
+            this.DateTimeDogum.CalendarMonthBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.DateTimeDogum.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.DateTimeDogum.Location = new System.Drawing.Point(428, 92);
+            this.DateTimeDogum.Name = "DateTimeDogum";
+            this.DateTimeDogum.Size = new System.Drawing.Size(324, 34);
+            this.DateTimeDogum.TabIndex = 2;
+            // 
+            // ComboCins
+            // 
+            this.ComboCins.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ComboCins.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ComboCins.FormattingEnabled = true;
+            this.ComboCins.Location = new System.Drawing.Point(784, 89);
+            this.ComboCins.Name = "ComboCins";
+            this.ComboCins.Size = new System.Drawing.Size(165, 37);
+            this.ComboCins.TabIndex = 3;
+            // 
+            // ListPersonel
+            // 
+            this.ListPersonel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ListPersonel.FormattingEnabled = true;
+            this.ListPersonel.ItemHeight = 29;
+            this.ListPersonel.Location = new System.Drawing.Point(28, 132);
+            this.ListPersonel.Name = "ListPersonel";
+            this.ListPersonel.Size = new System.Drawing.Size(1196, 294);
+            this.ListPersonel.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(973, 89);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(223, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 29);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(455, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 29);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Doğum Yılı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(779, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 29);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Cinsiyet";
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSoyad.Location = new System.Drawing.Point(228, 92);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(162, 34);
+            this.txtSoyad.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ListPersonel);
+            this.Controls.Add(this.ComboCins);
+            this.Controls.Add(this.DateTimeDogum);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Personel İşlemleri";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.DateTimePicker DateTimeDogum;
+        private System.Windows.Forms.ComboBox ComboCins;
+        private System.Windows.Forms.ListBox ListPersonel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSoyad;
+    }
+}
+
---
+++ ToolBoxKullanimiVeDigerOzellikler/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework;
+
+namespace ToolBoxKullanimiVeDigerOzellikler
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ComboCins.Items.Add("Bay");
+            ComboCins.Items.Add("Bayan");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Deger = txtAd.Text + " " + txtSoyad.Text + " " + DateTimeDogum.Text + " " + ComboCins.Text; // İlkel Yol
+
+            string Deger2 = $@"{txtAd.Text} {txtSoyad.Text} {DateTimeDogum.Text} {ComboCins.Text}"; // Modern Yol
+
+            string Ad, Soyad, DogumYili, Cinsiyet; // Parçalı İşleme Yolu
+            DateTime DY = Convert.ToDateTime(DateTimeDogum.Value);
+            Ad = txtAd.Text;
+            Soyad = txtSoyad.Text;
+            DogumYili = DY.ToShortDateString();
+            Cinsiyet = ComboCins.Text;
+            ListPersonel.Items.Add($@"{Ad} {Soyad} {DogumYili} {Cinsiyet}");
+        }
+    }
+}
